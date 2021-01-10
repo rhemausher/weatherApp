a7cea32d43f55ad7edb6d2b975987f04478837ca
Mark Jones <rhemausher@gmail.com>
2021-01-09 19:32:26 -0500

    Added even more useful text to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -12,7 +12,7 @@ const forecast = (lat, lon, callback) => {
                 callback(body.error, undefined)
             } else {
                 callback(undefined,
-                   body.current.weather_descriptions[0] + ':  It is ' + body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike + ' degrees out.  There is a ' + body.current.precip + '% chance of rain' + '  Humidity is ' + body.current.humidity + '  Visibility is ' + body.current.visibility
+                   body.current.weather_descriptions[0] + ':  It is ' + body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike + ' degrees out.  There is a ' + body.current.precip + '% chance of rain' + '  Humidity is ' + body.current.humidity + ' degrees  Visibility is ' + body.current.visibility + ' miles'
                 )
             }
         })
